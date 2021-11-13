ee2fcd86d35a79a40b61e0e09f75d2037bd0a599
Taeil Kwak <Taeil2@gmail.com>
2021-11-13 08:30:19 -0800

    null error

--- components/results.tsx
+++ components/results.tsx
@@ -25,8 +25,10 @@ const Results = () => {
 
     // {"trend":[{"period":"0m","strongBuy":2,"buy":1,"hold":3,"sell":1,"strongSell":0},{"period":"-1m","strongBuy":2,"buy":5,"hold":1,"sell":0,"strongSell":0},{"period":"-2m","strongBuy":2,"buy":5,"hold":1,"sell":0,"strongSell":0},{"period":"-3m","strongBuy":2,"buy":1,"hold":4,"sell":0,"strongSell":0}],"maxAge":86400}
     const yahooBuysString = text.match(/{"trend":(.*?)"maxAge":[0-9]+}/gm)
-    const yahooBuysJson = JSON.parse(yahooBuysString[0])
-    console.log(yahooBuysJson)
+    if (yahooBuysString !== null) {
+      const yahooBuysJson = JSON.parse(yahooBuysString[0])
+      console.log(yahooBuysJson)
+    }
     console.log('fetching');
   }
 
